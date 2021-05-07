884141deeee1303bd3b12c2023fb096c153e3053
David Royer <droyer01@gmail.com>
2021-05-06 20:52:00 -0400

    refactor: makes middleware logic easier to follow

--- middleware/route-controller.js
+++ middleware/route-controller.js
@@ -1,9 +1,10 @@
 export default ({ store, redirect, route }) => {
   const activeUser = store.state.user
+  const isTodosRoute = route.name === 'todos'
 
   // If user NOT logged in & trying to access protected route => Redirect to Home
-  if (!activeUser && route.name === 'todos') redirect('/')
+  if (!activeUser && isTodosRoute) redirect('/')
 
   // If user IS logged in & not at Todos route => Redirect to Todos
-  if (activeUser && route.name !== 'todos') redirect('/todos')
+  if (activeUser && !isTodosRoute) redirect('/todos')
 }
